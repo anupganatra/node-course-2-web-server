3c0d7dabeb7a0f7baf4c2009eca00573df843364
Anup Ganatra <anup.ganatra@infostretch.com>
2019-04-24 16:14:24 +0530

    Capitilize port command

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -51,5 +51,5 @@ app.get('/bad', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log('Server is up on port 3000');
+    console.log(`Server is up on port ${port}`);
 });
